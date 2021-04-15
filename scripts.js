86a2bb84c70d7701508f4b1a853d4d90691f7fb7
llaminator <llaminator@users.noreply.github.com>
2021-04-15 19:33:51 +0300

    questionable code style, but kind of works

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
-let buttonActive = "button-active";
-let buttonDisabled = "button-disabled";
-let buttonDivId = "button_div";
+let buttonActiveStyle = 'button-active';
+let buttonDisabledStyle = 'button-disabled';
+
+let buttonDivId = 'buttonDiv';
+let buttonType = 'button';
+let buttonValue = 'Click me';
+
+let phoneErrorId = 'phoneError';
+let phoneDiv = 'phoneDiv';
+
+let spanTag = 'span'
+let pTag = 'p';
+
+let fileInputsId = 'fileInputs';
+let submitForm = 'submitForm';
+
 let phoneRegExp = /^\+[0-9] \([0-9]{3}\) [0-9]{3}-[0-9]{2}-[0-9]{2}$/
 let amountOfRequiredImages = 2;
 
 
+
+
 function createNewButton(newButtonId) {
-    let newButton = document.createElement("input");
-    newButton.type = "button";
-    newButton.value = "Click me";
+    let newButton = document.createElement('input');
+
+    newButton.type = buttonType;
+    newButton.value = buttonValue;
     newButton.onclick = function() { spawnTwoButtons(newButtonId) };
-    newButton.className = buttonActive;
+    newButton.className = buttonActiveStyle;
     newButton.id = newButtonId;
 
     return newButton;
 }
 
 function calculateLastId() {
-    let activeButtons = document.getElementsByClassName(buttonActive);
+    let activeButtons = document.getElementsByClassName(buttonActiveStyle);
     let curMaxId = 1;
+
     for (let i = 0; i < activeButtons.length; i++) {
         if (activeButtons[i].id > curMaxId) {
             curMaxId = activeButtons[i].id;
         }
     }
+
     return curMaxId;
 }
 
 function spawnTwoButtons(buttonId) {
     let clickedButton = document.getElementById(buttonId)
+    console.log(clickedButton);
     let lastGivenId = calculateLastId();
 
     let firstNewButton = createNewButton(lastGivenId + 1);
@@ -38,8 +57,8 @@ function spawnTwoButtons(buttonId) {
     document.getElementById(buttonDivId).appendChild(secondNewButton);
 
     clickedButton.disabled = true;
-    clickedButton.className = buttonDisabled;
-    clickedButton.value = "";
+    clickedButton.className = buttonDisabledStyle;
+    clickedButton.value = '';
 
 }
 
@@ -50,31 +69,33 @@ function checkPhoneMask(phoneNumber) {
     console.log();
 
     if (phoneNumberExists == null) {
-        let phoneErrorExists = document.getElementById("phoneError");
+        let phoneErrorExists = document.getElementById(phoneErrorId);
 
         if (phoneErrorExists == null) {
-            let phoneErrorSpan = document.createElement("span");
-            phoneErrorSpan.id = "phoneError";
+            let phoneErrorSpan = document.createElement(spanTag);
+            phoneErrorSpan.id = phoneErrorId;
 
-            let phoneErrorText = document.createElement("p");
-            phoneErrorText.textContent = "Please fill in the number properly";
+            let phoneErrorText = document.createElement(pTag);
+            phoneErrorText.textContent = 'Please fill in the number properly';
 
-            document.getElementById("phone-div").appendChild(phoneErrorSpan);
+            document.getElementById(phoneDiv).appendChild(phoneErrorSpan);
             phoneErrorSpan.appendChild(phoneErrorText);
         }
+        validateInputs();
     } else {
-        if (document.getElementById("phoneError") != null) {
-            document.getElementById("phoneError").remove();
+        if (document.getElementById(phoneErrorId) != null) {
+            document.getElementById(phoneErrorId).remove();
         }
+        validateInputs();
     }
 
 }
 
 function countFiles() {
-    let fileInputs = document.getElementById("file-inputs").childNodes;
+    let fileInputs = document.getElementById(fileInputsId).childNodes;
     let cnt = 0;
     for (let i = 0; i < fileInputs.length; i++) {
-        if (fileInputs[i].type === "file") {
+        if (fileInputs[i].type === 'file') {
             cnt++;
         }
     }
@@ -82,47 +103,35 @@ function countFiles() {
 }
 
 function createNewFileInput() {
-    let fileInputs = document.getElementById("file-inputs");
-
-    let newInput = document.createElement("input");
-    newInput.type = "file";
+    let fileInputs = document.getElementById(fileInputsId);
+    console.log(fileInputs);
+    let newInput = document.createElement('input');
+    newInput.type = 'file';
     console.log(countFiles());
 
     if (countFiles() < amountOfRequiredImages) {
         newInput.required = true;
     }
 
-    if (countFiles() + 1 >= amountOfRequiredImages) {
-        let submitButton = document.getElementById("submitForm");
-        submitButton.disabled = false;
-    } else {
-        let submitButton = document.getElementById("submitForm");
-        submitButton.disabled = true;
-    }
-
     fileInputs.appendChild(newInput);
+    validateInputs();
 }
 
 
 function removeLastFileInput() {
-    document.getElementById("file-inputs").lastChild.remove();
-
-    if (countFiles() >= amountOfRequiredImages) {
-        let submitButton = document.getElementById("submitForm");
-        submitButton.disabled = false;
-    } else {
-        let submitButton = document.getElementById("submitForm");
-        submitButton.disabled = true;
-    }
+    document.getElementById(fileInputsId).lastChild.remove();
+    validateInputs();
 }
 
 function validateInputs() {
-    let phoneError = document.getElementById("phoneError");
+    let phoneError = document.getElementById(phoneErrorId);
+    console.log(phoneError);
     //fact that files are attached in at least two of file inputs is checked on the spot when initializing them
     if (phoneError == null && countFiles() >= amountOfRequiredImages) {
-
-        let successRes = document.createElement("p1");
-        successRes.innerText = "Form has been submitted successfully, redirect or whatever";
-        document.body.appendChild(successRes);
+        let submitButton = document.getElementById(submitForm);
+        submitButton.disabled = false;
+    } else {
+        let submitButton = document.getElementById(submitForm);
+        submitButton.disabled = true;
     }
 }
